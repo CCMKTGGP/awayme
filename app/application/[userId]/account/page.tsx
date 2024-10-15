3596f19aa571aa7f120b49886457ff942a02349b
--show-origin <shreynpatel23@gmail.com>
2024-10-15 19:02:49 -0400

    Add timezone feature in user details update.

--- app/application/[userId]/account/page.tsx
+++ app/application/[userId]/account/page.tsx
@@ -6,62 +6,86 @@ import Input from "@/app/components/input";
 import Button from "@/app/components/button";
 import React, { useState, useEffect } from "react";
 import { useUserContext } from "@/context/userContext";
-import { postData, putData } from "@/utils/fetch";
+import { putData } from "@/utils/fetch";
+import { getAllTimezones } from "@/utils/time";
+import Dropdown from "@/app/components/dropdown";
+import ApiError from "@/app/components/api-error";
 
 export default function Account() {
-    const { user, setUser, toggleFetchUserDetails, setToggleFetchUserDetails } = useUserContext();
-    const [firstName, setFirstName] = useState(user.firstName);
-    const [lastName, setLastName] = useState(user.lastName);
-    const [error, setError] = useState({
-      firstNameError: "",
-      lastNameError: ""
-    });
-    const [isLoading, setIsLoading] = useState(false);
-  
-  
-    const handleUpdate = async () => {
-      setIsLoading(true);
-      if (validateFields()) {
-        try {
-          const url = `/api/users/${user._id}`; 
-          const body = { firstName, lastName };
-          const updatedUser = await putData(url, body);
-          setUser(updatedUser); 
-          setToggleFetchUserDetails(!toggleFetchUserDetails);
-          console.log("Update successful:", updatedUser);
-        } catch (error: any) {
-          console.error("Failed to update user details:", error.message);
-          setError({
-            firstNameError: error.message || "Update failed",
-            lastNameError: error.message || "Update failed"
-          });
-        } finally {
-          setIsLoading(false);
-        }
-      } else {
+  const { user, setUser, toggleFetchUserDetails, setToggleFetchUserDetails } =
+    useUserContext();
+  const [timezones, setTimezones] = useState<any[]>([]);
+  const [selectedTimezone, setSelectedTimezone] = useState(user?.timeZone);
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [error, setError] = useState({
+    firstNameError: "",
+    lastNameError: "",
+    timeZoneError: "",
+    apiError: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const { tzNames } = getAllTimezones();
+    setTimezones(tzNames?.map((tz) => ({ id: tz, name: tz })));
+  }, []);
+
+  const handleUpdate = async () => {
+    setIsLoading(true);
+    if (validateFields()) {
+      try {
+        const url = `/api/users/${user._id}`;
+        const body = { firstName, lastName, timeZone: selectedTimezone };
+        const updatedUser = await putData(url, body);
+        setUser(updatedUser);
+        setToggleFetchUserDetails(!toggleFetchUserDetails);
+      } catch (error: any) {
+        console.error("Failed to update user details:", error.message);
+        setError((err: any) => ({
+          ...err,
+          apiError: err?.message,
+        }));
+      } finally {
         setIsLoading(false);
       }
-    };
-  
-    const handleCancel = () => {
-      setFirstName(user.firstName);
-      setLastName(user.lastName);
-      setError({ firstNameError: "", lastNameError: "" }); 
-    };
-  
-    function validateFields() {
-      let isValid = true;
-      setError({ firstNameError: "", lastNameError: "" });
-      if (!firstName) {
-        setError(prev => ({ ...prev, firstNameError: "First name is required" }));
-        isValid = false;
-      }
-      if (!lastName) {
-        setError(prev => ({ ...prev, lastNameError: "Last name is required" }));
-        isValid = false;
-      }
-      return isValid;
+    } else {
+      setIsLoading(false);
     }
+  };
+
+  const handleCancel = () => {
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+    setError((error) => ({
+      ...error,
+      firstNameError: "",
+      lastNameError: "",
+      timeZoneError: "",
+    }));
+  };
+
+  function validateFields() {
+    let isValid = true;
+    setError((error) => ({
+      ...error,
+      firstNameError: "",
+      lastNameError: "",
+      timeZoneError: "",
+    }));
+    if (!firstName) {
+      setError((prev) => ({
+        ...prev,
+        firstNameError: "First name is required",
+      }));
+      isValid = false;
+    }
+    if (!lastName) {
+      setError((prev) => ({ ...prev, lastNameError: "Last name is required" }));
+      isValid = false;
+    }
+    return isValid;
+  }
 
   return (
     <div className="flex items-start">
@@ -73,7 +97,7 @@ export default function Account() {
             <h3 className="font-archivo text-2xl leading-[48px] text-heading font-semibold">
               Account
             </h3>
-            <div className="flex items-center gap-6 w-full">
+            <div className="flex items-start gap-6 w-full">
               <div className="w-[50%]">
                 <Input
                   type="text"
@@ -106,6 +130,30 @@ export default function Account() {
               value={user.email}
               disabled
             />
+            <Dropdown
+              id="selectTimeZone"
+              label="Select Time Zone"
+              isDisabled={isLoading}
+              onClick={(value) => setSelectedTimezone(value?.id)}
+              options={timezones}
+              selectedOption={{
+                id: selectedTimezone,
+                name: selectedTimezone,
+              }}
+              hasError={error.timeZoneError !== ""}
+              error={error.timeZoneError}
+            />
+            {error.apiError && (
+              <ApiError
+                message={error.apiError}
+                setMessage={(value) =>
+                  setError((error) => ({
+                    ...error,
+                    apiError: value,
+                  }))
+                }
+              />
+            )}
             <div className="flex items-center gap-8 mt-4">
               <Button
                 buttonText="Cancel"
@@ -118,6 +166,7 @@ export default function Account() {
                 buttonClassName="rounded-md shadow-button hover:shadow-buttonHover bg-accent text-white"
                 onClick={handleUpdate}
                 isLoading={isLoading}
+                isDisabled={isLoading}
               />
             </div>
           </div>
